00075eb33cdb8f4b4585398707b9ac094d9e19e9
我发个呆 <12588276+im-in-a-daze@user.noreply.gitee.com>
2024-11-04 19:07:43 +0800

    第二次修改

--- 1.cpp
+++ 1.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, HYT!" << std::endl;  // 修改了输出内容
+    std::cout << "Hello, HYT————fwb!" << std::endl;  // 修改了输出内容
+    
     return 0;
 }
